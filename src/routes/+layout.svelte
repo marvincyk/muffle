c8a51d30432d51ea51597f5df101fa5754946017
Marvin Chew <marvin@marvincyk.com>
2024-08-11 13:45:26 +1000

    Add fade page transition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
-<script>
+<script lang="ts">
 	import "../app.css";
+	import { fade } from "svelte/transition";
+
+	export let data;
 </script>
 
-<slot />
+{#key data.url}
+	<div in:fade={{ duration: 300, delay: 400 }} out:fade={{ duration: 300 }}>
+		<slot />
+	</div>
+{/key}
