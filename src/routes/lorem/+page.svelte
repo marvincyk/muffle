5bbf43f341fd0054542bfde428bf8b2110d9fe91
Marvin Chew <marvin@marvincyk.com>
2024-08-11 13:34:18 +1000

    Add responsive padding to Lorem ipsum

--- src/routes/lorem/+page.svelte
+++ src/routes/lorem/+page.svelte
@@ -4,6 +4,6 @@
 	const lorem = new LoremIpsum({}, "plain", "\n\n");
 </script>
 
-<p class="text-justify p-20 leading-loose whitespace-pre-line">
+<p class="text-justify p-10 lg:p-20 leading-loose whitespace-pre-line">
 	{lorem.generateParagraphs(100)}
 </p>
