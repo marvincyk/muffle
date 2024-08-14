5f6976e8822983265459eecfaf0db2428e8060aa
Marvin Chew <marvin@marvincyk.com>
2024-08-14 22:47:01 +1000

    Update hero description

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,8 +14,8 @@
 			<h1 class="text-9xl font-bold">muffle</h1>
 			<p class="py-6">
 				A book reader designed to help you focus by encouraging a quieter reading environment. As
-				ambient noise increases, the page gradually darkens or distorts, prompting you to find a
-				quieter space to read.
+				background noise increases, the page gradually distorts, prompting you to find a quieter
+				space to read.
 			</p>
 			<a role="button" class="btn btn-ghost btn-lg btn-outline" href="/lorem">Begin Reading</a>
 		</div>
