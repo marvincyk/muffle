3ada2c44b4366c4d9a887fd67a26a2f88e6ead21
Marvin Chew <marvin@marvincyk.com>
2024-08-16 18:29:32 +1000

    Render all PDF pages

--- src/lib/components/PDFViewer.svelte
+++ src/lib/components/PDFViewer.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { onMount } from "svelte";
+	import { onMount, onDestroy } from "svelte";
 	import {
 		getDocument,
 		GlobalWorkerOptions,
@@ -7,13 +7,12 @@
 	} from "pdfjs-dist/legacy/build/pdf.mjs";
 	import pdfjsWorker from "pdfjs-dist/build/pdf.worker?url";
 
-	let canvas: HTMLCanvasElement;
-	let pageNum = 1;
 	let pdfDoc: PDFDocumentProxy | null = null;
+	let pages: number[] = [];
 
 	GlobalWorkerOptions.workerSrc = pdfjsWorker;
 
-	async function renderPage(num: number) {
+	async function renderPage(num: number, canvas: HTMLCanvasElement) {
 		if (!pdfDoc) return;
 
 		const page = await pdfDoc.getPage(num);
@@ -32,29 +31,35 @@
 		}
 	}
 
-	function goToPrevPage() {
-		if (pageNum > 1) {
-			pageNum--;
-			renderPage(pageNum);
-		}
-	}
-
-	function goToNextPage() {
-		if (pdfDoc && pageNum < pdfDoc.numPages) {
-			pageNum++;
-			renderPage(pageNum);
-		}
-	}
-
 	onMount(async () => {
 		const pdfUrl = "lorem.pdf";
 		pdfDoc = await getDocument(pdfUrl).promise;
-		renderPage(pageNum);
+		const numPages = pdfDoc.numPages;
+
+		pages = Array.from({ length: numPages }, (_, i) => i + 1);
+
+		requestAnimationFrame(() => {
+			pages.forEach((page) => {
+				const canvas = document.querySelector(`#page-${page}`) as HTMLCanvasElement;
+				if (canvas) {
+					renderPage(page, canvas);
+				}
+			});
+		});
+	});
+
+	onDestroy(() => {
+		pages.forEach((page) => {
+			const canvas = document.querySelector(`#page-${page}`);
+			if (canvas) {
+				canvas.remove();
+			}
+		});
 	});
 </script>
 
-<div class="flex justify-center">
-	<button on:click={goToPrevPage} class="flex-1 cursor-pointer"></button>
-	<canvas bind:this={canvas}></canvas>
-	<button on:click={goToNextPage} class="flex-1 cursor-pointer"></button>
+<div class="flex flex-col gap-4 p-4 overflow-auto">
+	{#each pages as page (page)}
+		<canvas id={`page-${page}`} class="mb-4 w-full"></canvas>
+	{/each}
 </div>
